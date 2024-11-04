8ea188b2b60d03ee8696702b53d3b6ad168ba64a
Matteo Valenzi <dilloawallace@hotmail.com>
2024-11-04 21:16:47 +0100

    ripasso moduli

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,21 @@
-const p: string = "Pluto";
-console.log(p);
-console.log(p);
-console.log("Ciao");
+// import { somma, sottrazione } from "./matematica";
+import * as mate from "./matematica";
+import molt from "./matematica";
+import op, { somma, sottrazione } from "./matematica";
+
+let frutta = ["mela", "pera", "banana"];
+
+// alternativa 1
+// for (let i = 0; i < frutta.length; i++) {
+//     const f = frutta[i];
+//     console.log(f);
+// }
+
+// alternativa 2 (molto ma molto più bella)
+for (const f of frutta) {
+    console.log(f);
+}
+
+console.log(somma(3, 5));
+console.log(mate.sottrazione(4, 9));
+console.log(molt(5, 7));
---
+++ src/matematica.ts
@@ -0,0 +1,13 @@
+export const somma = (a: number, b: number) => {
+    return a + b;
+}
+
+export const sottrazione = (a: number, b: number) => {
+    return a - b;
+}
+
+const moltiplicazione = (a: number, b: number) => {
+    return a * b;
+}
+
+export default moltiplicazione;
